485fe23b6ed08c77c4912b461ced0437f18769b0
chenyuan <chenyuanluck@163.com>
2016-11-21 14:38:27 +0800

    整理目录

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require('path');
+// 分离css插件
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+// 分离css插件配置
+let extractSCSS = new ExtractTextPlugin('stylesheets/[name].css');
+module.exports = {
+    devtool: 'inline-source-map',                                   // 启用source-map
+    entry: './src/scripts/main.js',                                 // 入口文件
+    debug: true,
+    output: {
+        path: path.join(__dirname, 'main'),                         // 打包输出的路径
+        filename: 'main.js',                                      // 打包输出的文件名
+        publicPath: './main/'                                       // html引用路径
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.scss$/,
+                // loader: extractSCSS.extract(['css?sourceMap', 'sass?sourceMap'])
+                loader: 'style!css?sourceMap!sass?sourceMap'
+            }, {
+                test: /\.(es6)|(js)$/,
+                loader: 'babel?presets[]=es2015'
+                // plugins: ['babel-plugin-components'],
+                // query: {
+                //     presets: ['es2015']
+                // }
+            }
+        ]
+    },
+    plugins: [
+        extractSCSS
+    ]
+};
